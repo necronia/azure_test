6121aa9fc6b609ffbd570f5902585ed0a80ae794
necronia <necronia44@gmail.com>
2020-11-25 08:15:45 +0000

    9th

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello .... Plz????!!!!!");
+    response.end("Hello! ");
 });
 
 const port = process.env.PORT || 3000;
